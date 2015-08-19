012fcdae9d11b9aa0e8fbf86e3cabe1793a198f1
Carlos Galdino <carloshsgaldino@gmail.com>
2015-08-19 13:54:39 -0300

    Add third exercise.

---
+++ ex03/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    let mut stdin = io::stdin();
+    let mut buffer = String::new();
+
+    println!("What is the quote?");
+    stdin.read_line(&mut buffer).unwrap();
+
+    println!("Who said it?");
+    stdin.read_line(&mut buffer).unwrap();
+
+    let output = buffer.lines().last().unwrap().to_string() + " says, " + "\"" + buffer.lines().nth(0).unwrap() + "\"";
+
+    println!("{}", output);
+}
