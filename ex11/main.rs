2785c97c6934cb32b667d94b3615a2b125b0399e
Carlos Galdino <carloshsgaldino@gmail.com>
2015-08-24 21:17:07 -0300

    Add eleventh exercise.

---
+++ ex11/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+use std::process;
+
+fn main() {
+    let mut stdin  = io::stdin();
+    let mut buffer = String::new();
+
+    println!("How many euros are you exchanging?");
+    stdin.read_line(&mut buffer).unwrap();
+    println!("What is the exchange rate?");
+    stdin.read_line(&mut buffer).unwrap();
+
+    let amount = match buffer.lines().nth(0).unwrap().parse::<f64>() {
+        Ok(x) => x,
+        Err(_) => {
+            println!("You must enter a number.");
+            process::exit(1);
+        }
+    };
+
+    let rate = match buffer.lines().nth(1).unwrap().parse::<f64>() {
+        Ok(x) => x,
+        Err(_) => {
+            println!("You must enter a number.");
+            process::exit(1);
+        }
+    };
+
+    println!("{} euros at an exchange rate of {} is\n{:.2} USD", amount, rate, amount * rate / 100.00);
+}
