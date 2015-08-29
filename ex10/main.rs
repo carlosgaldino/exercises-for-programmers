4455609447485c717394287699826b3d6483095c
Carlos Galdino <carloshsgaldino@gmail.com>
2015-08-29 16:04:17 -0300

    Change values to `f64`.

--- ex10/main.rs
+++ ex10/main.rs
@@ -1,8 +1,8 @@
 use std::io;
 use std::process;
 
-fn parse_input(x: &str) -> i32 {
-    match x.parse::<i32>() {
+fn parse_input(x: &str) -> f64 {
+    match x.parse::<f64>() {
         Ok(n) => n,
         Err(_) => {
             println!("You must enter only numbers.");
@@ -30,11 +30,11 @@ fn main() {
     println!("Enter the quantity of item 3: ");
     stdin.read_line(&mut buffer).unwrap();
 
-    let items: Vec<i32> = buffer.lines().map(parse_input).collect();
-    let price           = items.chunks(2).map(|xs| xs.into_iter().fold(1, |acc, n| acc * n)).fold(0, |acc, n| acc + n);
+    let items: Vec<f64> = buffer.lines().map(parse_input).collect();
+    let price           = items.chunks(2).map(|xs| xs.into_iter().fold(1.0, |acc, n| acc * n)).fold(0.0, |acc, n| acc + n);
     let tax             = price as f64 * 0.055;
 
-    println!("Subtotal: ${}", price);
-    println!("Tax: ${}", tax);
-    println!("Total: ${}", price as f64 + tax);
+    println!("Subtotal: ${:.2}", price);
+    println!("Tax: ${:.2}", tax);
+    println!("Total: ${:.2}", price as f64 + tax);
 }
