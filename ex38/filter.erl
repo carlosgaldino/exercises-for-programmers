e1ad28afaad6d263b9bf71dfbe0fbd409ff1f69c
Carlos Galdino <carloshsgaldino@gmail.com>
2016-08-05 00:08:57 -0300

    Fix ex38.

--- ex38/filter.erl
+++ ex38/filter.erl
@@ -2,17 +2,17 @@
 -export([start/0]).
 
 start() ->
-    Input = re:replace(io:get_line("Enter a list of numbers, separated by spaces: "), "(\\s+)", "", [global,{return,list}]),
+    Input = re:replace(io:get_line("Enter a list of numbers, separated by spaces: "), "^\\s+|\\s+$", "", [global,{return, list}]),
     Res = lists:filtermap(fun(X) ->
-                               case string:to_integer([X]) of
-                                   {Y, _} when is_integer(Y) ->
-                                       case Y rem 2 of
-                                           0 -> {true, Y};
-                                           _ -> false
-                                       end;
-                                   _ ->
-                                       false
+                                  case string:to_integer(X) of
+                                      {Y, _} when is_integer(Y) ->
+                                          case Y rem 2 of
+                                              0 -> {true, Y};
+                                              _ -> false
+                                          end;
+                                      _ ->
+                                          false
                                end
                        end,
-                       Input),
+                       string:tokens(Input, " ")),
     io:format("The even numbers are ~s.~n", [lists:join(" ", [integer_to_list(X) || X <- Res])]).
